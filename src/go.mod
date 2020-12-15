6bc562389014643a9b3b8613752b20ffe5d2bb70
Hamid Teimouri <hamidteimouri@outlook.com>
2020-12-15 15:25:56 +0330

    Initial commit

---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module github.com/hamidteimouri/go-hexagonal-architecture
+
+go 1.13
---
+++ src/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("** Balance service is running **")
+
+	fmt.Println("** Balance service has been stopped **")
+}
